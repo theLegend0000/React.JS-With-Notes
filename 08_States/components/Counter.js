2409aaaae6a1faabbc2bf718191b2b8fec64c4e8
theLegend0000 <chemistryguider79@gmail.com>
2026-03-22 20:01:35 +0500

    08 Updated

--- 08_States/components/Counter.js
+++ 08_States/components/Counter.js
@@ -2,17 +2,19 @@ import { useState } from "react"
 
 const Counter = () => {
 
-    const [count ,setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-    // const myStateArr = useState(0)
-    // const count = myStateArr[0]
-    //const setCount = myStateArr[1]
+  // const myStateArr = useState(0)
+  // const count = myStateArr[0]
+  //const setCount = myStateArr[1]
   return (
     <>
       <h1>Counter App</h1>
       <h1>{count}</h1>
-      <button onClick={()=> {
-        setCount(count + 1)
+      <button onClick={() => {
+        // setCount(count + 1)
+        setCount(prev => prev + 1)
+        
       }}>Count</button>
     </>
   )
